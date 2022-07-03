b1fd85565c3fe0a8bac2272eb7249a48d1e47bc8
Doctware <yusclever9@gmail.com>
2022-07-03 01:45:48 -0700

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,8 @@
  * main - founction needs to return integer at the end of the execution
  *
  *Return: return as success and 1 as fail
- */
+*/
 
 
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}
+int main(void) { puts("\"programming is like building a multilingual puzzle");
+	
