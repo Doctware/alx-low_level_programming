000e44acdfdc6c1b74a23feb7cfa04e4453b7377
Doctware <yusclever9@gmail.com>
2022-07-03 01:27:48 -0700

    The puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - founction needs to return integer at the end of the execution
+ *
+ *Return: return as success and 1 as fail
+ */
+
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle ");
+	return (0);
+}
