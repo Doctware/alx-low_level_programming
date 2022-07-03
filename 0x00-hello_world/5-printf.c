e82708dac595cec9202ab125a095a5a0e39cd03b
Doctware <yusclever9@gmail.com>
2022-07-03 02:58:21 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - founction need to return an integer after execution
+ *
+ * Return: return 0 as sucess and 1 as fail
+ */
+
+int main(void);
+{
+	printf("with proper grammer, but the outcome is a pice of art,\n");
+	return (0);
+}
