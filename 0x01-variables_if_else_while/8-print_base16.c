c513b52be20518190c04bdfa5374e134ba51c19a
Doctware <yusclever9@gmail.com>
2022-07-04 17:29:37 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Discription: print all numbers of base i6 in lowercase
+ * Return: rerturn 0 as success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i = '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
