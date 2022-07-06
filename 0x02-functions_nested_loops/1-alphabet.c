d9c4910369dc96bbe840bad1b7e79cd1deab9344
Doctware <yusclever9@gmail.com>
2022-07-06 07:13:03 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print alohabet of letter
+ *
+ * Return: @ void
+ *
+ */
+
+void print_alphabet(void)
+
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z' i++);
+
+	{
+		_putchar(i);
+	}
+
+		_putchar('\n')
+
+}
