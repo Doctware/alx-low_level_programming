230f4d9096ae3c3ef131f1309358f8afddcb8c61
Doctware <yusclever9@gmail.com>
2022-07-07 09:46:48 -0700

    on debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests founction print if integer positive or nagative
+ *Return: 0 as success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
