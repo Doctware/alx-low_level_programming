795b24a51942dbc179e502672c079a3468633ae7
Doctware <yusclever9@gmail.com>
2022-07-12 13:05:29 -0700

    The swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - founcion that swap two inegers
+ * @a: change to value of b
+ * @b: change to the value of a
+ * Return: @ void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
