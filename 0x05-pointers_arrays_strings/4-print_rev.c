bb3c87d220efec9f89fba1e7a5718c4fd3b7177c
Doctware <yusclever9@gmail.com>
2022-07-12 14:20:24 -0700

    'print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - this founction print string in reverse
+ * @s: parneter for adress
+ * Return: 0 as success
+ */
+
+void print_rev(char *s)
+{
+	int count = 0
+
+		while (s[count])
+		{
+			count++;
+		}
+
+			count--;
+
+			for (; count >= 0; count--)
+			{
+				_putchar(s[count]);
+			}
+
+			_putchar('\n');
+
+}
