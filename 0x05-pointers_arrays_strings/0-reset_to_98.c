2e08578583df21383bc4db6b9d92ccfb631808a2
Doctware <yusclever9@gmail.com>
2022-07-12 12:33:52 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - this founction takes pointer to an input
+ * @n: pointer with the adress value
+ * Return: @ 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The pionters arrays & string
