1d8e55f936dde0344440b7e15bde41144a757dd3
Doctware <yusclever9@gmail.com>
2022-07-12 13:42:56 -0700

    STR lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - founction that print the srting length
+ * @s: pointer with adress value
+ * Retur: @ void
+ */
+
+int _strlen(char *s)
+{
+	int e = 0;
+
+	while (*s != '\0')
+	{
+		e++;
+		s++;
+	}
+
+	return (e);
+}
