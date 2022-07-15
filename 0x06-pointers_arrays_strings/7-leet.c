186b89a8fd05c9fade3d885aed455241af52807d
Doctware <yusclever9@gmail.com>
2022-07-14 19:33:42 -0700

    Fiix strcat

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - to encode a string to 1337
+ * @c: string
+ * Return: string that is ecoded
+ */
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+
+			/* 32 is the diffrence between lower case letters and upper case letter*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
+}
