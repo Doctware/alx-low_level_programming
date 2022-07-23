a5eaa555d1bb0140e00b1723f1d5ff5c116a8fa3
Doctware <yusclever9@gmail.com>
2022-07-22 17:32:30 -0700

    1 args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print the number of argument passed int it
- * @argc: number of commands line argument
- * @argv: array that contains the program command line argument
- * Return: 0 as success
+ * main - print the number of argument passed int it.
+ * @argc: number of commands line argument.
+ * @argv: array that contains the program command line argument.
+ * Return: 0 as success.
  */
 
 int main(int argc, char argv[] __attribute__((unused)))
