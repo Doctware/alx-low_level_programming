a5cc1cbecd5187576d7c5092dc1f78f65d167719
Doctware <yusclever9@gmail.com>
2022-07-22 17:18:34 -0700

    No going back

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the number of argument passed int it
+ * @argc: number of commands line argument
+ * @argv: array that contains the program command line argument
+ * Return: 0 as success
+ */
+
+int main(int argc, char argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
