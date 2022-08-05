8d35ce2bd3ba790fbcc3df8ab4c2bf39a306718c
Doctware <yusclever9@gmail.com>
2022-08-04 23:29:53 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - sum all arguments
  * @n: int, number of undefined arguments
  * Return: sum of args
- * on error, o is returned
+ * on error, o is returned.
  */
 int sum_them_all(const unsigned int n, ...)
 {
