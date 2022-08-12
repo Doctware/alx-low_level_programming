c88b96c6f412359980c1b86319d696864c753f5a
Doctware <yusclever9@gmail.com>
2022-08-11 21:41:59 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,6 +29,12 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free (new_node);
+		return (NULL);
+	}
+	new_node->len = c;
 	new_node->next = NULL;
 
 	if (current)
