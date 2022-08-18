61287857e37b91646ca286c644413b5e764e6c09
Doctware <yusclever9@gmail.com>
2022-08-18 10:31:16 -0700

    Get the endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checkes the endianness.
+ *
+ *
+ * Return: If big-endia - 0
+ *  If little-endian - 1
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
