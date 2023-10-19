f2cfda84b63bf8954de05195c36cbb4baee16340
Doctware <yusclever9@gmail.com>
2023-10-19 13:41:10 +0100

    I love integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer to be printed
+ * Return: Success
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
