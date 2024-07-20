b9e9e14b49f01d8af07853060a0c16feff03bc6d
Doctware <yusclever9@gmail.com>
2024-07-21 00:03:23 +0100

    My header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,10 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOD
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGO_H
 
+#include <stddef.h>
+#include <stdlib.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
-
 
 #endif /* search algorithim */
